533353b039140cbe0f57e652ece97ca43b7d291b
Micah Kary <mikary@microsoft.com>
2015-02-10 18:09:00 -0800

    WIP - Creating Multigraph

---
+++ src/EntityFramework.Core/Utilities/Multigraph.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Microsoft.Data.Entity.Utilities
+{
+    public class Multigraph<TVertex, TEdge> : Graph<TVertex>
+    {
+        private readonly HashSet<TVertex> _verticies = new HashSet<TVertex>();
+        private readonly HashSet<TEdge> _edges = new HashSet<TEdge>();
+        private readonly Dictionary<TVertex, HashSet<TVertex>> _successorMap = new Dictionary<TVertex, HashSet<TVertex>>();
+
+        public IEnumerable<TEdge> Edges
+        {
+            get { return _edges; }
+        }
+
+        public virtual IEnumerable<TEdge> GetIncomingEdges([NotNull] TVertex to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IEnumerable<TEdge> GetOutgoingEdges([NotNull] TVertex from)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void AddVertex([NotNull] TVertex vertex)
+        {
+            Check.NotNull(vertex, nameof(vertex));
+
+            _verticies.Add(vertex);
+        }
+
+        public virtual void AddVertices([NotNull] IEnumerable<TVertex> verticies)
+        {
+            Check.NotNull(verticies, nameof(verticies));
+
+            _verticies.UnionWith(verticies);
+        }
+
+        public virtual void AddEdge(TVertex from, TVertex to, TEdge edge)
+        {
+            HashSet<TVertex> successors;
+            if (!_successorMap.TryGetValue(from, out successors))
+            {
+                successors = new HashSet<TVertex>();
+                _successorMap.Add(from, successors);
+            }
+            successors.Add(to);
+
+            _edges.Add(edge);
+        }
+
+        public override IEnumerable<TVertex> Vertices
+        {
+            get { return _verticies; }
+        }
+
+        public override IEnumerable<TVertex> GetOutgoingNeighbours([NotNull]TVertex from)
+        {
+            HashSet<TVertex> successorSet;
+            if (_successorMap.TryGetValue(from, out successorSet))
+            {
+                return successorSet;
+            }
+            return Enumerable.Empty<TVertex>();
+        }
+
+        public override IEnumerable<TVertex> GetIncomingNeighbours([NotNull]TVertex to)
+        {
+            return _successorMap.Where(kvp => kvp.Value.Contains(to)).Select(kvp => kvp.Key);
+        }
+    }
+}
---
+++ test/EntityFramework.Core.Tests/Utilities/MultigraphTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.Internal;
+using Microsoft.Data.Entity.Utilities;
+using Xunit;
+
+namespace Microsoft.Data.Entity.Tests.Utilities
+{
+    public class MultigraphTest
+    {
+        #region Fixture
+
+        private class Vertex
+        {
+            public int Id { get; set; }
+        }
+
+        private class Edge
+        {
+            public int Id { get; set; }
+        }
+
+        #endregion
+
+        [Fact]
+        public void AddVertex_adds_a_vertex()
+        {
+            var vertexOne = new Vertex { Id = 1 };
+            var vertexTwo = new Vertex { Id = 2 };
+
+            var graph = new Multigraph<Vertex, Edge>();
+
+            graph.AddVertex(vertexOne);
+            graph.AddVertex(vertexTwo);
+
+            Assert.Equal(2, graph.Vertices.Count());
+            Assert.Equal(2, graph.Vertices.Intersect(new[] { vertexOne, vertexTwo }).Count());
+        }
+
+        [Fact]
+        public void AddVertex_throws_on_null_vertex()
+        {
+            Vertex vertex = null;
+
+            var graph = new Multigraph<Vertex, Edge>();
+
+            Assert.Equal(
+                "vertex",
+                Assert.Throws<ArgumentNullException>(() => graph.AddVertex(vertex)).ParamName);
+        }
+
+        [Fact]
+        public void AddVertices_add_verticies()
+        {
+            var vertexOne = new Vertex { Id = 1 };
+            var vertexTwo = new Vertex { Id = 2 };
+            var vertexThree = new Vertex { Id = 3 };
+
+            var graph = new Multigraph<Vertex, Edge>();
+
+            graph.AddVertices(new[] { vertexOne, vertexTwo });
+            graph.AddVertices(new[] { vertexTwo, vertexThree });
+
+            Assert.Equal(3, graph.Vertices.Count());
+            Assert.Equal(3, graph.Vertices.Intersect(new[] { vertexOne, vertexTwo, vertexThree }).Count());
+        }
+
+        [Fact]
+        public void AddVerices_throws_on_null_vertices()
+        {
+            IEnumerable<Vertex> vertices = null;
+
+            var graph = new Multigraph<Vertex, Edge>();
+
+            Assert.Equal(
+                "verticies",
+                Assert.Throws<ArgumentNullException>(() => graph.AddVertices(vertices)).ParamName);
+        }
+
+        [Fact]
+        public void AddEdge_adds_an_edge()
+        {
+            var vertexOne = new Vertex { Id = 1 };
+            var vertexTwo = new Vertex { Id = 2 };
+            var vertexThree = new Vertex { Id = 3 };
+
+            var edgeOne = new Edge { Id = 1 };
+            var edgeTwo = new Edge { Id = 2 };
+            var edgeThree = new Edge { Id = 3 };
+
+            var graph = new Multigraph<Vertex, Edge>();
+            graph.AddEdge(vertexOne, vertexTwo, edgeOne);
+            graph.AddEdge(vertexOne, vertexThree, edgeTwo);
+            graph.AddEdge(vertexTwo, vertexThree, edgeThree);
+
+            Assert.Equal(3, graph.Edges.Count());
+            Assert.Equal(3, graph.Edges.Intersect(new[] { edgeOne, edgeTwo, edgeThree }).Count());
+
+            Assert.Equal(2, graph.GetOutgoingNeighbours(vertexOne).Count());
+            Assert.Equal(2, graph.GetOutgoingNeighbours(vertexOne).Intersect(new[] { vertexTwo, vertexThree }).Count());
+
+            Assert.Equal(2, graph.GetIncomingNeighbours(vertexThree).Count());
+            Assert.Equal(2, graph.GetIncomingNeighbours(vertexThree).Intersect(new[] { vertexOne, vertexTwo }).Count());
+        }
+    }
+}
